f605332e187d469ae62b4d85f323480f77a5528d
animesh-jsr <animesh975sunny@gmail.com>
2025-04-26 02:01:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: 
+    { fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    
+      colors: {
+        deepBlue:"#02042a",
+        lightBlue300: "#4b9aed",       
+        lightBlue: "#2b84ea",       
+        lightBlue500: "#0b72e7",       
+        greenLight: "#90ee90",
+        grayText: "#6b7280",
+        lightGrey: "#d3d3d3",
+        grayBlue: "#5f9ea0",
+        deepBlueAhead: "#001f3f",    
+        gray2: "#a9a9a9",
+
+      },
+     },
+  },
+  plugins: [],
+}
+
